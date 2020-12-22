1e7af2e2d140a1991b1b8337dbf79218938cc6e5
Алексей <52876090+AlexeyVodopyanov@users.noreply.github.com>
2020-12-22 10:36:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            WriteLine("Четное или нечетное число");
+            Write("Введите число ");
+            int a;
+            a = Convert.ToInt32(ReadLine());
+            if (a % 2 == 0)
+            {
+                WriteLine($"{a} это четное");
+            }
+            else
+            {
+                WriteLine($"{a} это нечетное");
+            }
+        }
+    }
+}
